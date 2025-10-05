39d5324a11688ffc77adf71f04ab9817c7d34045
Daniel <dansusnation@gmail.Com>
2025-10-05 15:35:37 -0400

    fix: rename CorsConfig bean to avoid conflict

--- backend/src/main/java/com/olympics/tickets/backend/config/CorsConfig.java
+++ backend/src/main/java/com/olympics/tickets/backend/config/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Bean
+    @Bean(name = "corsConfigurerBackend") // renommer le bean pour éviter le conflit
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
